8bbd917389ee693411c67342d8044975ba9e34f1
MeshackKorir <meshackkorir100@gmail.com>
2024-01-25 16:16:51 +0300

    instance section

--- dist/product.js
+++ dist/product.js
@@ -1,4 +1,41 @@
 "use strict";
+// let id = document.querySelector('#name') as HTMLInputElement
+let body = document.querySelector(".body");
+let image = document.querySelector('#profile');
+let names = document.querySelector('#name');
+let author = document.querySelector('#author');
+let title = document.querySelector('#title');
+let date_established = document.querySelector('#date_established');
 // let buttOnonClick = document.querySelector('#buttononclick') as HTMLButtonElement;
+// buttOnonClick.addEventListener("click", (() =>{
+// }))
+// let body = document.querySelector('.body') as HTMLFormElement;
+let currentIndex;
 // Initializing an empty array
 let Books = [];
+body.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let book = names.value.trim() != "" && image.value.trim() != "" && author.value.trim() != "" && title.value.trim() != "" && date_established.value.trim() != "";
+    if (book) {
+        let bookDetails = {
+            id: Books.length + 1,
+            names: names.value.trim(),
+            image: image.value.trim(),
+            author: author.value.trim(),
+            title: title.value.trim(),
+            date_established: date_established.value.trim()
+        };
+        if (currentIndex) {
+            Books.splice(currentIndex, 1, bookDetails);
+        }
+        else {
+            Books.push(bookDetails);
+        }
+        // instance.displayBooks()
+        names.value = "";
+        image.value = "";
+        author.value = "";
+        title.value = "";
+        date_established.value = "";
+    }
+});
--- src/product.ts
+++ src/product.ts
@@ -1,3 +1,11 @@
+// let id = document.querySelector('#name') as HTMLInputElement
+let body = document.querySelector(".body") as HTMLFormElement
+let image = document.querySelector('#profile') as HTMLInputElement
+let names = document.querySelector('#name') as HTMLInputElement
+let author = document.querySelector('#author') as HTMLInputElement
+let title = document.querySelector('#title') as HTMLInputElement
+let date_established = document.querySelector('#date_established') as HTMLInputElement
+
 // let buttOnonClick = document.querySelector('#buttononclick') as HTMLButtonElement;
 
 // buttOnonClick.addEventListener("click", (() =>{
@@ -6,18 +14,53 @@
 
 // let body = document.querySelector('.body') as HTMLFormElement;
 
+let currentIndex:number;
 
 interface Book{
     id: number;
     image: string;
-    name: string;
+    names: string;
     author: string;
     title: string;
-    date_established: number
+    date_established: string
 }
 
 // Initializing an empty array
 let Books: Book[]=[]
+
+body.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    let book = names.value.trim() != "" && image.value.trim() != "" && author.value.trim() != "" && title.value.trim() != "" && date_established.value.trim() != ""
+
+    if(book){
+        let bookDetails = {
+            id: Books.length + 1,
+            names: names.value.trim(),
+            image: image.value.trim(),
+            author: author.value.trim(),
+            title: title.value.trim(),
+            date_established: date_established.value.trim()
+        };
+
+    
+        if(currentIndex){
+            Books.splice(currentIndex, 1, bookDetails)
+
+
+        }else{
+            Books.push(bookDetails)
+        }
+
+        // instance.displayBooks()
+
+        names.value = ""
+        image.value = ""
+        author.value = ""
+        title.value = ""
+        date_established.value = ""
+    }
+})
 
 
 
