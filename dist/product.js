df3c56847249c7247de273519538d19defe53a68
MeshackKorir <meshackkorir100@gmail.com>
2024-01-25 06:34:07 +0300

    ecommerce

---
+++ dist/product.js
@@ -0,0 +1,7 @@
+"use strict";
+const navbar = document.querySelector('.navbar');
+class theApp {
+    constructor() {
+        this.products = [];
+    }
+}
---
+++ src/product.ts
@@ -0,0 +1,29 @@
+const navbar = document.querySelector('.navbar') as HTMLDivElement;
+
+interface Product{
+    id: number;
+    url: string;
+
+}
+
+
+
+
+
+// local storage feature
+
+// const getLocalStorageItem => (key) {
+//     return JSON.parse(window.localStorage.getItem(key))
+//   }
+  
+//   const setLocalStorageItem => (key, value) {
+//     window.localStorage.setItem(key, JSON.stringify(value));
+//   }
+
+
+  // 
+
+
+
+
+
