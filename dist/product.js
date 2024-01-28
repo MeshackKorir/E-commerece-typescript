52613d38b3ce5182f9c34919a0add9c7ad088ced
MeshackKorir <meshackkorir100@gmail.com>
2024-01-28 06:49:40 +0300

    manufacture

--- dist/product.js
+++ dist/product.js
@@ -1,23 +1,25 @@
 "use strict";
-let id = document.querySelector('#name');
+// let id = document.querySelector('#name') as HTMLInputElement
 let body = document.querySelector(".body");
 let image = document.querySelector('#profile');
-let names = document.querySelector('#name');
+let names = document.querySelector('#names');
 let author = document.querySelector('#author');
 let title = document.querySelector('#title');
-let date_established = document.querySelector('#date_established');
-// let buttOnonClick = document.querySelector('#buttononclick') as HTMLButtonElement;
-// buttOnonClick.addEventListener("click", (() =>{
-// }))
+let date_established = document.querySelector('#date');
+let profiles = document.querySelector('.profiless');
+let buttonOnClick = document.querySelector('#button');
+buttonOnClick.addEventListener("click", (() => {
+}));
 // let body = document.querySelector('.body') as HTMLFormElement;
 let currentIndex;
 // Initializing an empty array
 let Books = [];
 body.addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log(names.value);
     let book = names.value.trim() != "" && image.value.trim() != "" && author.value.trim() != "" && title.value.trim() != "" && date_established.value.trim() != "";
     if (book) {
-        let bookDetails = {
+        let newBookDetails = {
             id: Books.length + 1,
             names: names.value.trim(),
             image: image.value.trim(),
@@ -25,13 +27,15 @@ body.addEventListener("submit", (e) => {
             title: title.value.trim(),
             date_established: date_established.value.trim()
         };
+        console.log(newBookDetails);
+        Books.push(newBookDetails);
         if (currentIndex) {
-            Books.splice(currentIndex, 1, bookDetails);
+            Books.splice(currentIndex, 1, newBookDetails);
         }
         else {
-            Books.push(bookDetails);
+            Books.push(newBookDetails);
         }
-        // instance.displayBooks()
+        instance.displayBooks();
         names.value = "";
         image.value = "";
         author.value = "";
@@ -39,3 +43,60 @@ body.addEventListener("submit", (e) => {
         date_established.value = "";
     }
 });
+class BooksActions {
+    displayBooks() {
+        Books.forEach((book, index) => {
+            let profiles = document.createElement('tr');
+            profiles.className = "profiless";
+            let numbering = document.createElement('td');
+            numbering.textContent = `${index + 1}`;
+            let name = document.createElement('td');
+            name.textContent = book.names;
+            let image = document.createElement('td');
+            image.textContent = book.image;
+            let author = document.createElement('td');
+            author.textContent = book.author;
+            let title = document.createElement('td');
+            title.textContent = book.title;
+            let date_established = document.createElement('td');
+            date_established.textContent = book.date_established;
+            let deletebtn = document.createElement('button');
+            deletebtn.textContent = "Delete";
+            deletebtn.style.backfaceVisibility = 'red';
+            deletebtn.addEventListener('click', () => {
+                this.deleteBook(index);
+            });
+            let updatebtn = document.createElement('button');
+            updatebtn.textContent = "Update";
+            updatebtn.style.backfaceVisibility = 'skyblue';
+            updatebtn.addEventListener('click', () => {
+                this.updateBook(index);
+            });
+            profiles.appendChild(names);
+            profiles.appendChild(image);
+            profiles.appendChild(name);
+            profiles.appendChild(author);
+            profiles.appendChild(date_established);
+            profiles.appendChild(title);
+            profiles.appendChild(deletebtn);
+            profiles.appendChild(updatebtn);
+        });
+    }
+    deleteBook(index) {
+        Books.splice(index, 1);
+        this.displayBooks();
+    }
+    updateBook(index) {
+        currentIndex = index;
+        console.log(currentIndex);
+        // createBookform.style.display = 'flex'
+        let user = Books[index];
+        names.value = user.names;
+        title.value = user.title;
+        author.value = user.author;
+        date_established.value = user.date_established;
+        image.value = user.image;
+    }
+}
+let instance = new BooksActions();
+instance.displayBooks();
--- src/product.ts
+++ src/product.ts
@@ -1,16 +1,16 @@
-let id = document.querySelector('#name') as HTMLInputElement
+// let id = document.querySelector('#name') as HTMLInputElement
 let body = document.querySelector(".body") as HTMLFormElement
 let image = document.querySelector('#profile') as HTMLInputElement
-let names = document.querySelector('#name') as HTMLInputElement
+let names = document.querySelector('#names') as HTMLInputElement
 let author = document.querySelector('#author') as HTMLInputElement
 let title = document.querySelector('#title') as HTMLInputElement
-let date_established = document.querySelector('#date_established') as HTMLInputElement
+let date_established = document.querySelector('#date') as HTMLInputElement
+let profiles = document.querySelector('.profiless')
+let buttonOnClick = document.querySelector('#button') as HTMLButtonElement;
 
-// let buttOnonClick = document.querySelector('#buttononclick') as HTMLButtonElement;
+buttonOnClick.addEventListener("click", (() =>{
 
-// buttOnonClick.addEventListener("click", (() =>{
-
-// }))
+}))
 
 // let body = document.querySelector('.body') as HTMLFormElement;
 
@@ -31,10 +31,12 @@ let Books: Book[]=[]
 body.addEventListener("submit", (e)=>{
     e.preventDefault()
 
+    console.log(names.value);
+
     let book = names.value.trim() != "" && image.value.trim() != "" && author.value.trim() != "" && title.value.trim() != "" && date_established.value.trim() != ""
 
     if(book){
-        let bookDetails = {
+        let newBookDetails = {
             id: Books.length + 1,
             names: names.value.trim(),
             image: image.value.trim(),
@@ -43,16 +45,18 @@ body.addEventListener("submit", (e)=>{
             date_established: date_established.value.trim()
         };
 
+        console.log(newBookDetails)
+        Books.push(newBookDetails)
     
         if(currentIndex){
-            Books.splice(currentIndex, 1, bookDetails)
+            Books.splice(currentIndex, 1, newBookDetails)
 
 
         }else{
-            Books.push(bookDetails)
+            Books.push(newBookDetails)
         }
 
-        // instance.displayBooks()
+        instance.displayBooks()
 
         names.value = ""
         image.value = ""
@@ -61,6 +65,94 @@ body.addEventListener("submit", (e)=>{
         date_established.value = ""
     }
 })
+
+
+class BooksActions{
+
+    displayBooks(){
+
+
+        Books.forEach((book: Book, index:number)=>{
+
+            let profiles = <HTMLTableRowElement >document.createElement('tr')
+            profiles.className = "profiless"
+
+            let numbering = document.createElement('td') as HTMLTableCellElement
+            numbering.textContent = `${index + 1}`
+
+            let name = document.createElement('td') as HTMLTableCellElement
+            name.textContent = book.names
+
+            let image = document.createElement('td') as HTMLTableCellElement
+            image.textContent = book.image
+
+            let author = document.createElement('td') as HTMLTableCellElement
+            author.textContent = book.author
+
+            let title = document.createElement('td') as HTMLTableCellElement
+            title.textContent = book.title
+
+            let date_established = document.createElement('td') as HTMLTableCellElement
+            date_established.textContent = book.date_established
+
+            let deletebtn = document.createElement('button') as HTMLButtonElement
+            deletebtn.textContent = "Delete"
+            deletebtn.style.backfaceVisibility = 'red'
+            deletebtn.addEventListener('click', ()=>{
+                this.deleteBook(index)
+            })
+
+            let updatebtn = document.createElement('button') as HTMLButtonElement
+            updatebtn.textContent = "Update"
+            updatebtn.style.backfaceVisibility = 'skyblue'
+            updatebtn.addEventListener('click', ()=>{
+                this.updateBook(index)
+            })
+
+            profiles.appendChild(names)
+            profiles.appendChild(image)
+            profiles.appendChild(name)
+            profiles.appendChild(author)
+            profiles.appendChild(date_established)
+            profiles.appendChild(title)
+            profiles.appendChild(deletebtn)
+            profiles.appendChild(updatebtn)
+
+          
+
+        })
+    }
+
+    deleteBook(index:number){
+        Books.splice(index, 1)
+
+        this.displayBooks()
+    }
+
+    updateBook(index:number){
+        currentIndex = index
+
+        console.log(currentIndex);
+        
+        // createBookform.style.display = 'flex'
+
+        let user = Books[index]
+
+        names.value = user.names
+        title.value = user.title
+        author.value = user.author
+        date_established.value = user.date_established
+        image.value = user.image
+    }
+}
+
+let instance = new BooksActions();
+
+instance.displayBooks()
+
+
+
+
 
 
 
