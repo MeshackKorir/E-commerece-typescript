6d7a719924d4f10bd6fdf05f86a1e6b184bd1e62
MeshackKorir <meshackkorir100@gmail.com>
2024-01-29 16:09:33 +0300

    rendering user side

--- dist/User.js
+++ dist/User.js
@@ -4,66 +4,42 @@
 // type HTMLDivElementWithQuerySelector = HTMLDivElement & { querySelector: typeof document.querySelector };
 // type HTMLInputElementWithQuerySelector = HTMLInputElement & { querySelector: typeof document.querySelector };
 // type HTMLSpanElementWithClassName = HTMLSpanElement & { className: string };
-// Initializing an empty array
-// let Books: Book[] = [];
-let divProduct = document.querySelector('.productsDiv');
-const user = JSON.parse(localStorage.getItem('books') || '[]');
-console.log(user);
-user.forEach((used, index) => {
-    const divy = document.createElement('div');
-    divy.innerHTML = `
-
-  <table class="profiless" >
-  <tr class="tableheading" style="gap: 15px;">
-      <th>No.</th>
-      <th>${used.names}</th>
-      <th>${used.image}</th>
-      <th>${used.author}</th>
-      <th>${used.title}</th>
-      <th>${used.date_established}</th>
-  </tr>
-</table>
-
-  
-  
-  
-  `;
-    divProduct.appendChild(divy);
-});
 class displayingProducts {
-    displayBooks() {
-        if (divProduct instanceof HTMLElement) {
-            divProduct.innerHTML = '';
-        }
-        Books.forEach((book, index) => {
-            let profiles = document.querySelector('.profiless');
-            if (profiles) {
-                let newRow = document.createElement('tr');
+    constructor() {
+        this.divProduct = document.querySelector('.productsDiv');
+    }
+    displayBooks(books) {
+        if (this.divProduct instanceof HTMLElement) {
+            this.divProduct.innerHTML = '';
+            books.forEach((book, index) => {
+                let newRow = document.createElement('div');
                 newRow.className = "profiless";
-                let numbering = document.createElement('td');
-                numbering.textContent = `${index + 1}`;
-                let name = document.createElement('td');
-                name.textContent = book.names;
-                let imageCell = document.createElement('td');
-                let imageElement = document.createElement('img');
-                imageElement.src = book.image;
-                imageElement.alt = 'Book Image';
-                imageCell.appendChild(imageElement);
-                let author = document.createElement('td');
-                author.textContent = book.author;
-                let title = document.createElement('td');
-                title.textContent = book.title;
-                let date_established = document.createElement('td');
-                date_established.textContent = book.date_established;
-                newRow.appendChild(numbering);
-                newRow.appendChild(name);
-                newRow.appendChild(imageCell);
-                newRow.appendChild(author);
-                newRow.appendChild(title);
-                newRow.appendChild(date_established);
-                divProduct.appendChild(newRow);
-            }
-        });
-        // this.saveToLocalStorage();
+                newRow.innerHTML = `
+          <table>
+            <tr class="tableheading" style="gap: 15px;">
+              
+              <th>${book.names}</th>
+              <th><img src="${book.image}" alt="Book Image"></th>
+              <th>${book.author}</th>
+              <th>${book.title}</th>
+              <th>${book.date_established}</th>
+            </tr>
+          </table>
+        `;
+                this.divProduct.appendChild(newRow);
+            });
+        }
+    }
+    loadFromLocalStorageAndDisplay() {
+        const storedBooks = localStorage.getItem('books');
+        if (storedBooks) {
+            const books = JSON.parse(storedBooks);
+            this.displayBooks(books);
+        }
     }
 }
+// Create an instance of displayingProducts
+let instances = new displayingProducts();
+window.onload = () => {
+    instances.loadFromLocalStorageAndDisplay();
+};
--- src/User.ts
+++ src/User.ts
@@ -44,143 +44,59 @@
 
 
 interface Book {
-  id: number;
-  image: string;
-  names: string;
-  author: string;
-  title: string;
-  date_established: string;
-}
-
-// Initializing an empty array
-// let Books: Book[] = [];
-
-let divProduct = document.querySelector('.productsDiv') as HTMLDivElement;
-
-const user = JSON.parse(localStorage.getItem('books') || '[]')
-
-console.log(user);
-
-
-user.forEach((used:Book ,index:number)=>{
-
-
-  const divy = document.createElement('div');
-
-  divy.innerHTML=`
-
-  <table class="profiless" >
-  <tr class="tableheading" style="gap: 15px;">
-      <th>No.</th>
-      <th>${used.names}</th>
-      <th>${used.image}</th>
-      <th>${used.author}</th>
-      <th>${used.title}</th>
-      <th>${used.date_established}</th>
-  </tr>
-</table>
-
-  
-  
-  
-  `
-
-
-
-
-
-  divProduct.appendChild(divy)
-
-})
-
-
-
-
-
-
-
-
-interface Book {
   names: string;
   image: string;
   author: string;
   title: string;
   date_established: string;
 }
-
-
 
 class displayingProducts {
+  private divProduct: HTMLDivElement;
 
-  displayBooks() {
-      if (divProduct instanceof HTMLElement) {
-        divProduct.innerHTML = '';
-      }
-  
-      Books.forEach((book: Book, index: number) => {
-          let profiles = document.querySelector('.profiless');
-          
-          if (profiles) {
-              let newRow = document.createElement('tr');
-              newRow.className = "profiless";
-
-              let numbering = document.createElement('td');
-              numbering.textContent = `${index + 1}`;
-
-              let name = document.createElement('td');
-              name.textContent = book.names;
-
-              let imageCell = document.createElement('td');
-              let imageElement = document.createElement('img');
-              imageElement.src = book.image;
-              imageElement.alt = 'Book Image';
-              imageCell.appendChild(imageElement);
-
-              let author = document.createElement('td');
-              author.textContent = book.author;
-
-              let title = document.createElement('td');
-              title.textContent = book.title;
-
-              let date_established = document.createElement('td');
-              date_established.textContent = book.date_established;
-
-
-
-              newRow.appendChild(numbering);
-              newRow.appendChild(name);
-              newRow.appendChild(imageCell);
-              newRow.appendChild(author);
-              newRow.appendChild(title);
-              newRow.appendChild(date_established);
-
-
-
-              divProduct.appendChild(newRow);
-
-          }
-      });
-
-      // this.saveToLocalStorage();
+  constructor() {
+    this.divProduct = document.querySelector('.productsDiv') as HTMLDivElement;
   }
 
-  // loadFromLocalStorage() {
-    // console.log("in function");
-    
-      // const storedBooks = localStorage.getItem('books');
-      // if (storedBooks) {
-      //     Books = JSON.parse(storedBooks);
-      //     console.log(Books);
-          
-      //     this.displayBooks();
-      // }
-  // }
-// }
+  displayBooks(books: Book[]) {
+    if (this.divProduct instanceof HTMLElement) {
+      this.divProduct.innerHTML = '';
 
-// let instances = new displayingProducts();
-// window.onload = () => { instances.loadFromLocalStorage(); };
+      books.forEach((book: Book, index: number) => {
+        let newRow = document.createElement('div');
+        newRow.className = "profiless";
 
+        newRow.innerHTML = `
+          <table>
+            <tr class="tableheading" style="gap: 15px;">
+              
+              <th>${book.names}</th>
+              <th><img src="${book.image}" alt="Book Image"></th>
+              <th>${book.author}</th>
+              <th>${book.title}</th>
+              <th>${book.date_established}</th>
+            </tr>
+          </table>
+        `;
 
+        this.divProduct.appendChild(newRow);
+      });
+    }
+  }
 
+  loadFromLocalStorageAndDisplay() {
+    const storedBooks = localStorage.getItem('books');
+    if (storedBooks) {
+      const books: Book[] = JSON.parse(storedBooks);
+      this.displayBooks(books);
+    }
+  }
 }
+
+// Create an instance of displayingProducts
+let instances = new displayingProducts();
+window.onload = () => {
+  instances.loadFromLocalStorageAndDisplay();
+};
+
 
